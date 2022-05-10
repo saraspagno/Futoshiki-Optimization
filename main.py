e4561b81b2be159a6efec291f292f3460d5a9de6
lilachHerzog <lilach.m.h@gmail.com>
2022-05-10 10:16:09 +0300

    changed constant_numbers to dict for easier access to the constant_numbers per row

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def parse_args(args_file):
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
-    constants.N = int(lines[index])
-    index += 1
+    constants.N = int(lines[0]) # the first line is the size of the matrix
+    lines.remove(lines[0])
+    constants.num_of_constants = int(lines[0]) # the second line is the number of given digits
 
-    constant_numbers = []
-    for i in range(int(lines[index])):
-        index += 1
-        line_values = list(map(int, lines[index].split(' ')))
-        constant_numbers.append(line_values)
-
-    index += 1
+    constant_numbers ={}
+    for constant_value_place in lines[0:constants.num_of_constants]:
+        line_values = list(map(int, constant_value_place.split(' ')))
+        constant_numbers[line_values[0]]= {line_values[1]:line_values[2]}
+        lines.remove(lines[0])
+#  change code to check if the key already has a value, and add the new value and not replace it
 
     greater_constraints = []
     for i in range(int(lines[index])):
