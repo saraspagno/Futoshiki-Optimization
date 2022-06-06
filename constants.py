cb722610cdf8a08f68394a8dfee16b18e81f21c7
sspagnoletto <sara_spagnoletto@intuit.com>
2022-06-06 22:33:58 +0300

    finished project

--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@ N = 5  # size of grid
 M = 100  # size of population
 
 G = 2  # weight of an error for not respecting "greater" constraint
-C = 2  # weight of an error for not respecting the "column" or "row" constraint
 
 MU = 0.5  # mutation probability
 STOP = 2  # after how many equal iterations to stop
